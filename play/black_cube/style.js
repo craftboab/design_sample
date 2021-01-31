3165ec8a4e1db78b080c200a0997fa8fe0ce7fd3
craftboab <75962893+craftboab@users.noreply.github.com>
2021-01-31 13:43:43 -0800

    third commit

---
+++ play/black_cube/style.js
@@ -0,0 +1,36 @@
+(() =>{
+
+let h = '<div class="row">'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '<div class="hexagon"></div>'
+        + '</div>';
+
+document.getElementById('cube_wrap').innerHTML = h;
+
+})();
---
+++ play/white_cube/style.js
@@ -0,0 +1,14 @@
+(() => {
+
+
+
+let $white_cube = document.getElementById('white_cube');
+
+function cube_multi(){
+  for(let i = 0; i<50; i++ ){
+    $white_cube.innerHTML('htmlshtnls');
+    // $white_cube.write('<span></span><span></span><span></span>');
+  }
+}
+
+})();
